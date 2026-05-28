6cdaf55f0853095730f54a2a1119f64b024b5f8f
Sudhanshu Koli <kolisudhanshu@gmail.com>
2026-05-28 14:11:59 +0530

    fixed border color issue

--- src/components/home/Dashboard.jsx
+++ src/components/home/Dashboard.jsx
@@ -2,6 +2,12 @@ import { motion } from "motion/react";
 
 export default function Dashboard(){
 
+    const borderColors = {
+        orange: "border-orange-500",
+        blue: "border-blue-500",
+        lime: "border-lime-500"
+    };
+
     return(<>
         <div className="m-auto p-10"> 
             <motion.div initial={{opacity: 0}} transition={{duration: 1, ease: "easeInOut"}} animate={{ opacity: 1 }} className="text-left">
@@ -18,7 +24,7 @@ export default function Dashboard(){
                 { label: "Tech Stacks", value: 5, icon: "🛠️", color: "lime" },
             ].map((stat, i) => (
                 <motion.div initial={{ y:40, opacity: 0}} transition={{duration: 0.8, delay: i*0.2,  ease: "easeInOut"}} animate={{y: 0, opacity: 1}}
-                            key={i} className={` bg-gray-800/30 border-t border-b rounded-2xl border-${stat.color}-500 p-10 w-50 m-auto md:w-100`}>
+                            key={i} className={` bg-gray-800/30 border-t border-b rounded-2xl ${borderColors[stat.color]} p-10 w-50 m-auto md:w-100`}>
                 <div style={{ fontSize: 28, marginBottom: 12 }}>{stat.icon}</div>
                 <div style={{ fontSize: 32, fontWeight: 800, color: stat.color }}>{stat.value}</div>
                 <div style={{ fontSize: 13, color: "#8B949E", marginTop: 4 }}>{stat.label}</div>
--- src/components/home/TechSelection.jsx
+++ src/components/home/TechSelection.jsx
@@ -3,6 +3,20 @@ import { motion } from "motion/react";
 import { STACKS } from "../../data/dashboardData/TechStack";
 
 export default function TechSelection(){
+
+    const borderColors = {
+        red: "border-red-600",
+        fuchsia: "border-fuchsia-600",
+        violet: "border-violet-600",
+        teal: "border-teal-600",
+        lime: "border-lime-600",
+        orange: "border-orange-600",
+        blue: "border-blue-600",
+        pink: "border-pink-600",
+        purple: "border-purple-600",
+        green: "border-green-600"
+    };
+
     return(<>
         {/* Stack Selection */}
         <div className="m-auto px-10"> 
@@ -12,7 +26,7 @@ export default function TechSelection(){
             <div className="grid grid-cols-4 gap-10 mb-10" >
             {STACKS.map((stack, i) => (
                 <motion.div initial={{y:20, opacity: 0}} animate={{ y:0, opacity: 1 }} transition={{  duration: 1, delay: i*0.2,  ease: "easeInOut"  }}
-                             key={stack.id} className={` bg-gray-800/30 border-t border-b border-${stack.color}-600 rounded-2xl p-10 `}
+                             key={stack.id} className={` bg-gray-800/30 border-t border-b ${borderColors[stack.color]} rounded-2xl p-10 `}
                 onClick={() => console.log("We have to create fun to start interview")}>
 
                     <motion.div animate={{ y: [0, -10, 0] }} transition={{  duration: 2,  repeat: Infinity, delay: i*0.2,  ease: "easeInOut"  }}
