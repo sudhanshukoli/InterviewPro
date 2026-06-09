2ed83e9fe66d55e9f859c8645a9e1a7560ddf0c6
Sudhanshu Koli <kolisudhanshu@gmail.com>
2026-06-09 12:17:27 +0530

    Made login and signup page mobile compatible

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -5,6 +5,7 @@ import { motion } from "motion/react";
 import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import { UserContext } from "../../context/UserContext";
+import bgLogin from "../../data/images/bgLogin.jpg";
 
 export default function Login(){
     
@@ -76,42 +77,43 @@ export default function Login(){
     }
 
     return(<>
-        <section className="text-white flex items-center justify-center bg-cover bg-linear-to-b from-blue-900/30 to-black w-full h-screen overflow-hidden shadow-xl">
-            <motion.div initial={{ y:  animateOut ? 0 : 800 }} transition={{duration: 0.5, ease:"easeInOut"}} animate={{ y: animateOut ? 800 : 0 }}
+        <section style={{backgroundImage:`linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(${bgLogin})`}}
+                className="text-white flex items-center justify-center bg-cover bg-linear-to-b from-blue-900/30 to-black w-full h-screen overflow-hidden shadow-xl">
+            <motion.div initial={{ y:  animateOut ? 0 : 800, opacity: animateOut ? 1 : 0 }} transition={{duration: 0.5, ease:"easeInOut"}} animate={{ y: animateOut ? 800 : 0, opacity: animateOut ? 0 : 1 }}
                         onAnimationComplete={ handleSignup }
-                        className="m-8 border rounded-2xl items-center  w-140 h-170 flex flex-col bg-white/5  border-white/20 shadow-2xl backdrop-blur-md ">
+                        className="m-8 border rounded-2xl items-center w-[80%] h-[60%]  md:w-140 md:h-170 flex flex-col  border-white/30 shadow-2xl backdrop-blur-sm ">
                 
-                <div className="flex">
-                    <div className="p-8 self-start">
-                        <p className="text-md text-left text-gray-400 ">Login your account</p>
-                        <h1 className="text-4xl font-bold ">Welcome Back!</h1>
-                        <p className="text-md text-left text-gray-400">Enter your username and password</p>
+                <div className="flex p-4 ">
+                    <div className="p-4 self-start">
+                        <p className=" text-sm md:text-md text-left text-gray-400 ">Login your account</p>
+                        <h1 className=" text-xl md:text-4xl text-left font-bold ">Welcome Back!</h1>
+                        <p className=" text-sm md:text-md text-left text-gray-400">Enter your username and password</p>
                     </div>
 
                     <div className="flex items-center">
-                        <FontAwesomeIcon className="text-2xl text-blue-500" icon={faBrain} /> 
-                        <h1 className="text-xl font-semibold text-transparent sm:text-2xl md:text-3xl  bg-linear-to-r from-blue-400 via-white to-blue-700 bg-clip-text"> InterviewPro</h1>
+                        <FontAwesomeIcon className=" text-sm md:text-2xl text-blue-500" icon={faBrain} /> 
+                        <h1 className="text-xl font-semibold text-transparent md:text-3xl  bg-linear-to-r from-blue-400 via-white to-blue-700 bg-clip-text"> InterviewPro</h1>
                     </div>
                 </div>    
 
-                <div className="flex flex-col items-center justify-center">
-                    <label htmlFor="username" className="self-start text-xl mb-2 text-gray-400">Username</label>
-                    <input className="float-left pl-12 mb-4 p-4 bg-transparent border rounded-2xl w-120 border-gray-400 border-opacity-700" 
+                <div className="flex flex-col items-center justify-center w-[80%]">
+                    <label htmlFor="username" className="self-start text-sm md:text-xl mb-2 text-gray-400">Username</label>
+                    <input className="float-left pl-12 mb-4 p-2 md:p-4 bg-transparent border rounded-2xl w-full md:w-120 border-gray-400 border-opacity-700" 
                             onChange={handleChange} value={loginData.username} type="text" name="username" placeholder="Enter your username" />
 
-                    <label htmlFor="password" className="self-start text-xl mb-2 text-gray-400">Password</label>
-                    <input onKeyDown={noSpace}  className="float-left pl-12 mb-10 p-4 bg-transparent border rounded-2xl w-120 border-gray-400 border-opacity-700"
+                    <label htmlFor="password" className="text-sm md:text-xl self-start mb-2 text-gray-400">Password</label>
+                    <input onKeyDown={noSpace}  className="float-left pl-12 mb-5 md:mb-10 p-2 md:p-4 bg-transparent border rounded-2xl w-full md:w-120 border-gray-400 border-opacity-700"
                             onChange={handleChange} value={loginData.password} type="password" name="password" placeholder="Enter your password" />
 
-                    <button disabled={isDisabled} className={`p-3 font-semibold ${isDisabled ? "cursor-not-allowed bg-gray-800" : "cursor-pointer bg-blue-900 from-blue-950 to-blue-500 hover:bg-linear-to-tr" } text-white w-120 rounded-2xl`} 
+                    <button disabled={isDisabled} className={`p-2 md:p-3 font-semibold ${isDisabled ? "cursor-not-allowed bg-gray-800" : "cursor-pointer bg-blue-900 from-blue-950 to-blue-500 hover:bg-linear-to-tr" } text-white w-full md:w-120 rounded-2xl`} 
                             onClick={handleLogin} >{loginClicked ? "Checking Creds...." : "Login"}</button>
                 </div>
 
-                <h1 className="text-xl text-gray-400 py-8">New here ? <button onClick={() => setAnimateOut(true)} className="underline cursor-pointer hover:text-white" >Register</button></h1>
+                <h1 className="text-sm md:text-xl text-gray-400 py-2 md:py-8">New here ? <button onClick={() => setAnimateOut(true)} className="underline cursor-pointer hover:text-white" >Register</button></h1>
 
-                <hr className="w-[50vh] my-2 border-0 h-0.5 bg-blue-800" />
+                <hr className="w-[50%] my-2 border-0 h-0.5 bg-blue-800" />
 
-                <p className="p-4"> <i className="font-semibold">Welcome back!</i> The dedication you are showing right now is exactly what will set you apart from every other candidate.</p>
+                <p className="p-4 text-sm md:text-xl"> <i className="font-semibold">Welcome back!</i> The dedication you are showing right now is exactly what will set you apart from every other candidate.</p>
             </motion.div>
         </section>
     </>)
--- src/components/auth/Signup.jsx
+++ src/components/auth/Signup.jsx
@@ -5,14 +5,15 @@ import { motion } from "motion/react";
 import { useContext, useState } from "react";
 import { useNavigate } from "react-router";
 import { UserContext } from "../../context/UserContext";
+import bgLogin from "../../data/images/bgLogin.jpg";
 
 export default function Signup(){
 
     const deployedUrl = "https://interviewprobackend-1.onrender.com";
     const { setUserData } = useContext(UserContext);
 
-    const inputClasses = "float-left pl-12 mb-4 p-4 bg-transparent border rounded-2xl w-120 border-gray-400 border-opacity-700";
-    const labelClasses = "self-start text-xl mb-2 text-gray-400";
+    const inputClasses = "float-left pl-12 mb-5 md:mb-4 p-2 md:p-4 bg-transparent border rounded-2xl w-full md:w-120 border-gray-400 border-opacity-700";
+    const labelClasses = "self-start text-sm md:text-xl mb-2 text-gray-400";
 
     const navigate = useNavigate();
 
@@ -94,23 +95,23 @@ export default function Signup(){
     }
 
     return(<>
-        <section className="text-white flex items-center justify-center bg-cover bg-linear-to-b from-blue-900/30 to-black w-full h-screen overflow-hidden shadow-xl">
-            <motion.div initial={{ y:  animateOut ? 0 : -800 }} transition={{duration: 0.5, ease:"easeInOut"}} animate={{ y: animateOut ? -800 : 0 }}
-                        onAnimationComplete={ handleLogin } className="m-8 border rounded-2xl items-center  w-140 h-170 flex flex-col bg-white/5  border-white/20 shadow-2xl backdrop-blur-md ">
-                <div className="flex">
-                    <div className="p-8 self-start">
-                        <p className="text-md text-left text-gray-400 ">START FOR FREE</p>
-                        <h1 className="text-2xl font-bold ">Create a new account.</h1>
-                        <p className="text-md text-left text-gray-400">Enter your details below</p>
+        <section style={{backgroundImage:`linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(${bgLogin})`}} className="text-white flex items-center justify-center bg-cover bg-linear-to-b from-blue-900/30 to-black w-full h-screen overflow-hidden shadow-xl">
+            <motion.div initial={{ y:  animateOut ? 0 : -800, opacity: animateOut ? 1 : 0 }} transition={{duration: 0.5, ease:"easeInOut"}} animate={{ y: animateOut ? -800 : 0, opacity: animateOut ? 0 : 1 }}
+                        onAnimationComplete={ handleLogin } className="m-8 border rounded-2xl items-center  w-[80%] h-[60%]  md:w-140 md:h-170 flex flex-col  border-white/20 shadow-2xl backdrop-blur-sm ">
+                <div className="flex p-4 ">
+                    <div className="p-4 self-start">
+                        <p className="text-sm md:text-md text-left text-gray-400 ">START FOR FREE</p>
+                        <h1 className="text-xl md:text-2xl text-left font-bold ">Create a new account.</h1>
+                        <p className="text-sm md:text-md text-left text-gray-400">Enter your details below</p>
                     </div>
 
                     <div className="flex items-center">
-                        <FontAwesomeIcon className="text-2xl text-blue-500" icon={faBrain} /> 
-                        <h1 className="text-xl font-semibold text-transparent sm:text-2xl md:text-3xl  bg-linear-to-r from-blue-400 via-white to-blue-700 bg-clip-text"> InterviewPro</h1>
+                        <FontAwesomeIcon className="text-sm md:text-2xl text-blue-500" icon={faBrain} /> 
+                        <h1 className="text-xl  font-semibold text-transparent sm:text-2xl md:text-3xl  bg-linear-to-r from-blue-400 via-white to-blue-700 bg-clip-text"> InterviewPro</h1>
                     </div>
                 </div>  
 
-                <div className="flex flex-col items-center justify-center">
+                <div className="flex flex-col items-center justify-center w-[80%]">
                     <label htmlFor="name" className={labelClasses}>Name</label>
                     <input className={inputClasses} onChange={handleChange} value={signupData.name} type="text" name="name" placeholder="Your name" />
 
@@ -124,11 +125,11 @@ export default function Signup(){
                     <label htmlFor="password" className={labelClasses}>Password</label>
                     <input onKeyDown={noSpace}  className={inputClasses} onChange={handleChange} value={signupData.password} type="password" name="password" placeholder="Password" />
 
-                    <button className={`p-3 font-semibold ${isDisabled ? "cursor-not-allowed bg-gray-800" : "cursor-pointer bg-blue-900 from-blue-950 to-blue-500 hover:bg-linear-to-tr" } text-white w-120 rounded-2xl`} 
+                    <button className={`p-2 md:p-3 font-semibold ${isDisabled ? "cursor-not-allowed bg-gray-800" : "cursor-pointer bg-blue-900 from-blue-950 to-blue-500 hover:bg-linear-to-tr" } text-white w-full md:w-120 rounded-2xl`} 
                             disabled={isDisabled} onClick={handleSignup} >{signupClicked ? "Creating account...." : "Signup"}</button>
                 </div>
 
-                <h1 className="text-xl text-gray-400 py-8">Already have an account ? <button onClick={() => setAnimateOut(true)} className="underline cursor-pointer hover:text-white" >Login</button></h1>
+                <h1 className="text-sm md:text-xl text-gray-400 py-2 md:py-8">Already have an account ? <button onClick={() => setAnimateOut(true)} className="underline cursor-pointer hover:text-white" >Login</button></h1>
             </motion.div>
         </section>
     </>)
